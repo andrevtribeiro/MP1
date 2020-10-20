c39d764dd634540cf05c5cfb34fc3a76a5d83a78
ritacsjfernandes <43547205+ritacsjfernandes@users.noreply.github.com>
2020-10-20 11:58:43 +0100

    script

---
+++ script.py
@@ -0,0 +1,17 @@
+dev = open('DEV.txt', 'r')
+dev_questions = open('DEV-questions.txt', 'w')
+dev_labels = open('DEV-labels.txt', 'w')
+
+questions = []
+labels = []
+lines = dev.readlines()
+
+for line in lines:
+    q = line.partition(" ")[2]
+    questions.append(q)
+    l = line.partition(" ")[0]
+    l = l + "\n"
+    labels.append(l)
+
+dev_questions.writelines(questions)
+dev_labels.writelines(labels)
